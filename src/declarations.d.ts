d90964795b03389e967f8ab51bbd5d35f4522685
ashish <ashish@nimblechapps.in>
2024-09-26 10:59:54 +0530

    remove dependancy

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,3 +3,10 @@ declare module '*.css' {
   const content: any;
   export default content;
 }
+
+declare module 'normalize-wheel' {
+  const normalizeWheel: (
+    event: WheelEvent
+  ) => { spinX: number; spinY: number; pixelX: number; pixelY: number }
+  export default normalizeWheel
+}
